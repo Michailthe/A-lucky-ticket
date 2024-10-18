de325d776d836eaa3ac706fddaf1f351bbf0b3f3
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-18 22:50:26 +0300

    Update Счастливый билетик.cpp

--- Счастливый билетик/Счастливый билетик.cpp
+++ Счастливый билетик/Счастливый билетик.cpp
@@ -1,27 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 int main() {
     int ticket_number, digits, sum_first_half = 0, sum_second_half = 0;
     cout << "Enter the ticket number (six digits): ";
     cin >> ticket_number;
 
-    // Calculate the sum of the first half and the second half of the ticket number
+// Рассчитайте сумму первой половины и второй половины номера билета
     for (int i = 0; i < 6; ++i) {
         digits = ticket_number % 10;
         ticket_number /= 10;
         if (i < 3) {
             sum_first_half += digits;
-        }
-        else {
+        } else {
             sum_second_half += digits;
         }
     }
 
-    // Check if the ticket is lucky
+// Проверьте, является ли билет счастливым.
     if (sum_first_half == sum_second_half) {
         cout << "Lucky ticket!" << endl;
-    }
-    else {
+    } else {
         cout << "Better luck next time!" << endl;
     }
 
